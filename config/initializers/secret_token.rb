0e92c679eaa3cf073654f4bec1ce1ca73a893b7e
Michaela Överfors <michaela.overfors@gmail.com>
2012-04-26 10:27:24 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AssociationForum::Application.config.secret_token = '56ca77a6ec90c49929be994e722b2f40eac5f3215779584a58b7dd78db7282de84cafb920f4d8ca4fcf56aefecad33d02346010eeeac14da4aa63cfd4b76150e'
