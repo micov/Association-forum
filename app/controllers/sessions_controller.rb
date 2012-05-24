58d8f3af88425c077bebb802cfc692ee89278669
Michaela Överfors <michaela.overfors@gmail.com>
2012-05-24 11:19:47 +0200

    deleted stuff

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,8 +2,8 @@ class SessionsController < ActionController::Base
 require 'casclient'
 require 'casclient/frameworks/rails/filter'
 
-before_filter CASClient::Frameworks::Rails::GatewayFilter, :only => :index
-before_filter CASClient::Frameworks::Rails::Filter, :except => [ :unprotected_action, :another_unprotected_action ]
+before_filter CASClient::Frameworks::Rails::GatewayFilter #, :only => :index
+before_filter CASClient::Frameworks::Rails::Filter #, :except => [ :unprotected_action, :another_unprotected_action ]
 
   def new
     CASClient::Frameworks::Rails::Filter.login_url(self)
@@ -12,8 +12,7 @@ before_filter CASClient::Frameworks::Rails::Filter, :except => [ :unprotected_ac
   end
 
   def destroy
-    #self.request.referer = root_path
-    CASClient::Frameworks::Rails::Filter.logout(self)
+    CASClient::Frameworks::Rails::Filter.logout(self, nil)
+    #redirect_to root_path
   end
-
 end
