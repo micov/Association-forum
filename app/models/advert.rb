ca04d36cb46cb4a4b4e6bdea9b7521e735028031
Klara <klaso084@student.liu.se>
2012-05-27 14:43:15 +0200

    Associations, with pages, created

--- app/models/advert.rb
+++ app/models/advert.rb
@@ -1,5 +1,5 @@
 class Advert < ActiveRecord::Base
-  attr_accessible :heading
+  attr_accessible :heading, association_id
   belongs_to :association
 
   validates :association_id, presence: true
--- app/models/association.rb
+++ app/models/association.rb
@@ -1,5 +1,5 @@
 class Association < ActiveRecord::Base
-  attr_accessible :info, :webPage
+  attr_accessible :name, :info, :webPage
   
   #"dependent: :destroy" ensures that all adverts of an association 
   #is removed if the assoc. is removed
@@ -7,6 +7,6 @@ class Association < ActiveRecord::Base
   
   validates :name,  presence: true, length: { maximum: 50 }
   validates :info,  presence: true, length: { maximum: 300 }
-  validates :web_page,  presence: true, length: { maximum: 70 }
+  validates :webPage,  presence: true, length: { maximum: 70 }
   #mer???????? typ unikt namn.
 end
